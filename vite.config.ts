46043aaf0750cd76e2198c70bbf1a59fe0a01411
Carter <Bridgescc@appstate.edu>
2025-11-03 22:14:14 -0500

    tweaks

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/carter-bridges-dev/',
+  base: '/carterbridges.dev/',
   build: {
     outDir: 'dist',
     sourcemap: true,
